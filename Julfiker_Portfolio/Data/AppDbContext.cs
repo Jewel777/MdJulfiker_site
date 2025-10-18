7791da0061689aab2987b610b469892f985d2bd0
Md Julfiker Ali Jewel <111831358+Jewel777@users.noreply.github.com>
2025-10-18 15:02:01 -0400

    Update AppDbContext.cs

--- Julfiker_Portfolio/Data/AppDbContext.cs
+++ Julfiker_Portfolio/Data/AppDbContext.cs
@@ -5,7 +5,10 @@ namespace Julfiker_Portfolio.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
 
         public DbSet<PageHit> PageHits => Set<PageHit>();
         public DbSet<DailyStat> DailyStats => Set<DailyStat>();
