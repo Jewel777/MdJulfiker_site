16a748f8ba4b09db034e85cc210900672618bbc0
Md Julfiker Ali Jewel <111831358+Jewel777@users.noreply.github.com>
2025-10-19 01:05:18 -0400

    Update EmailSettings.cs

--- Julfiker_Portfolio/Models/EmailSettings.cs
+++ Julfiker_Portfolio/Models/EmailSettings.cs
@@ -2,17 +2,14 @@ namespace Julfiker_Portfolio.Models
 {
     public class EmailSettings
     {
-        public string Host { get; set; } = "smtp.gmail.com";
-        public int    Port { get; set; } = 587;     // 587 StartTLS, 465 SSL
-        public bool   UseSsl { get; set; } = false; // false->StartTLS, true->SSL on connect
-        public string User { get; set; } = "";      // SMTP username (email)
-        public string Password { get; set; } = "";  // App password / SMTP password
-
-        public string FromEmail { get; set; } = ""; // your mailbox (sender)
-        public string FromName  { get; set; } = "Portfolio Contact";
-
-        public string ToEmail   { get; set; } = ""; // where you receive
-        public string ToName    { get; set; } = "Md Julfiker Ali Jewel";
+        public string Host { get; set; } = "";
+        public int Port { get; set; }
+        public bool UseSsl { get; set; }
+        public string User { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string FromEmail { get; set; } = "";
+        public string FromName { get; set; } = "";
+        public string ToEmail { get; set; } = "";
+        public string ToName { get; set; } = "";
     }
 }
-
