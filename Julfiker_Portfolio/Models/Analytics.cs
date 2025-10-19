5941a1e86620fec0459feca79b1e29f3ca87a1e8
Md Julfiker Ali Jewel <111831358+Jewel777@users.noreply.github.com>
2025-10-19 00:46:51 -0400

    Remove EmailSettings class from Analytics model
    Removed the EmailSettings class from Analytics.cs.

--- Julfiker_Portfolio/Models/Analytics.cs
+++ Julfiker_Portfolio/Models/Analytics.cs
@@ -25,18 +25,4 @@ namespace Julfiker_Portfolio.Models
         public int PageViews { get; set; }
         public int UniqueSessions { get; set; }
     }
-
-    // Keep your EmailSettings here if you already had it
-    public class EmailSettings
-    {
-        public string Host { get; set; } = "";
-        public int Port { get; set; }
-        public bool UseSsl { get; set; }
-        public string User { get; set; } = "";
-        public string Password { get; set; } = "";
-        public string FromEmail { get; set; } = "";
-        public string FromName { get; set; } = "";
-        public string ToEmail { get; set; } = "";
-        public string ToName { get; set; } = "";
-    }
 }
